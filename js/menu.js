7a7f9298664727ce0d22b227786e5d1bdeca368b
gianpier4598 <gianpier4598@gmail.com>
2024-04-26 06:57:08 -0300

    nuevos cambios pagina js

--- js/menu.js
+++ js/menu.js
@@ -1,17 +1,17 @@
 (function(){
     const openButton = document.querySelector('.nav__menu');
     const menu = document.querySelector('.nav__link');
-    const closeMenu = document.querySelectorAll('.nav__close');
+    const closeMenu = document.querySelector('.nav__close');
 
-
-
-    openButton.addEventListener('clic', ()=>{
-        menu.classList.add('nav:_link..show');
-       
+    openButton.addEventListener('click', ()=>{
+        menu.classList.add('nav__link--show');
     });
 
     closeMenu.addEventListener('click', ()=>{
         menu.classList.remove('nav__link--show');
     });
+
+
+
 
 })();
